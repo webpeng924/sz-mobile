32c97fbc36517b99f0ad6002ba042859071447f0
pyy <942142993@qq.com>
2020-08-11 15:14:46 +0800

    初始化

---
+++ src/views/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>123</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  methods: {},
+  created () { },
+  mounted () { },
+  watch: {},
+  computed: {}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
